bfeea59a61d45f6782a6fa3ee4c849f3c07ec4ca
Nikolas <nikolaslyko@gmail.com>
2018-03-19 19:22:37 +0300

    3 laba

--- NadApp/src/labwork3/DeterminingSystemReliabilityController.java
+++ NadApp/src/labwork3/DeterminingSystemReliabilityController.java
@@ -1,22 +1,18 @@
 package labwork3;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTabPane;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.ToggleGroup;
 import org.controlsfx.control.Notifications;
-import org.w3c.dom.events.Event;
 
-import java.beans.EventHandler;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class DeterminingSystemReliabilityController implements Initializable, Data {
+public class DeterminingSystemReliabilityController extends Laws implements Initializable, Data {
 
     @FXML
     private JFXTextField numTest;
@@ -32,6 +28,11 @@ public class DeterminingSystemReliabilityController implements Initializable, Da
 
     @FXML
     private JFXTabPane tabPane;
+
+    private Tab tab1 = new Tab("1-я группа");
+    private Tab tab2 = new Tab("2-я группа");
+    private Tab tab3 = new Tab("3-я группа");
+    private Tab tab4 = new Tab("4-я группа");
 
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
@@ -59,35 +60,94 @@ public class DeterminingSystemReliabilityController implements Initializable, Da
             }
         });
 
+        for (int i = 0; i < 15; i++) {
+            double a = Exponential(0.1);
+            double aa = Exponential(0.1);
+
+            double repairA = Exponential(0.1);
+            double repairAA = Exponential(0.1);
+
+            double max = Math.max(a, aa);
+            double min = Math.min(a, aa);
+
+//            do {
+//
+//
+//
+//            } while ( max > (min + repairA) | max > (min + repairAA) );
+//
+//            if (a < aa) {
+//                double firstElement = a + repairA;
+//                if (firstElement < aa) {
+//                    double fElement = firstElement + a;
+//                }
+//                else {
+//                    System.out.println("Система неисправна");
+//                }
+//            }
+//            else {
+//                double secondElement = aa + repairAA;
+//            }
+
+
+            double b = Rayleigh(20);
+            double bb = Rayleigh(20);
+            double bbb = Rayleigh(20);
+
+            double c = Normal(50, 10);
+            double cc = Normal(50, 10);
+
+            double swtch = Normal(100, 10);
+
+            System.out.println(a);
+            System.out.println(aa);
+            System.out.println("\n");
+
+        }
+
+
+
+
+
+
     }
 
     private void getThirdPane() {
-        Tab tab1 = new Tab("1-я группа");
-        Tab tab2 = new Tab("2-я группа");
-        Tab tab3 = new Tab("3-я группа");
+        ObservableList<String> laws = FXCollections.observableArrayList("Exponential", "Rayleigh", "Normal");
+        String s2 = "Rayleigh";
+        String s3 = "Normal";
 
-        tab1.setContent(new Button("fdgdrg"));
+        ToggleGroup toggleGroup = new ToggleGroup();
+        JFXRadioButton exp = new JFXRadioButton("exp");
+        exp.setToggleGroup(toggleGroup);
+
+        JFXRadioButton rel = new JFXRadioButton("rel");
+        rel.setToggleGroup(toggleGroup);
+
+        JFXRadioButton norm = new JFXRadioButton("norm");
+        norm.setToggleGroup(toggleGroup);
+
+        JFXComboBox jfxComboBox = new JFXComboBox<>(laws);
+
+        tab1.setContent(exp);
 
         if (tabPane.getTabs().size() < 2) {
             tabPane.getTabs().addAll(tab1, tab2, tab3);
         }
         else if (tabPane.getTabs().size() == 4) {
             tabPane.getTabs().clear();
-//            tabPane.getTabs().remove(0, 4);
             tabPane.getTabs().addAll(tab1, tab2, tab3);
         }
-
     }
 
     private void getFourthPane() {
         if (tabPane.getTabs().size() < 2) {
-            tabPane.getTabs().addAll(new Tab("1-я группа"), new Tab("2-я группа"), new Tab("3-я группа"), new Tab("4-я группа"));
+            tabPane.getTabs().addAll(tab1, tab2, tab3, tab4);
         }
         else if (tabPane.getTabs().size() == 3) {
-            tabPane.getTabs().remove(0, 3);
-            tabPane.getTabs().addAll(new Tab("1-я группа"), new Tab("2-я группа"), new Tab("3-я группа"), new Tab("4-я группа"));
+            tabPane.getTabs().clear();
+            tabPane.getTabs().addAll(tab1, tab2, tab3, tab4);
         }
-
     }
 
 }
---
+++ NadApp/src/labwork3/Laws.java
@@ -0,0 +1,31 @@
+package labwork3;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+class Laws {
+
+    private double genE() { return Math.random(); }
+
+    private double genY() { return (-1 + (Math.random() * 2)); }
+
+    double Exponential(double lambda) {
+
+        return -( 1 / lambda ) * Math.log(genE());
+
+    }
+
+    double Normal(int alpha, int sigma) {
+
+        return alpha + sigma * genY();
+
+    }
+
+    double Rayleigh(int sigma) {
+
+        double newE = new BigDecimal(genE()).setScale(3, RoundingMode.HALF_UP).doubleValue();
+        return sigma * Math.sqrt(-2 * Math.log(newE));
+
+    }
+
+}
